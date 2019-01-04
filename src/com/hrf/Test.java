4119938d0deec01e68c726244cdc160c6fac99cb
huangrongfei <huangrongfei@bjxcaifu.com>
2019-01-04 17:27:24 +0800

    333

--- src/com/hrf/Test.java
+++ src/com/hrf/Test.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Test {
 
     public static void main(String[] args){
-       //
+        System.out.println("1222224638946");
         BigDecimal decimal=new BigDecimal(15600);
         BigDecimal x=decimal.divideAndRemainder(BigDecimal.valueOf(100))[1];
         System.out.println(x.intValue()==0);
